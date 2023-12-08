ccb4104cff4a785628c32959bbe188d2fe6d04dc
Sebastian Myre <144124350+JegElskerFisk@users.noreply.github.com>
2023-12-08 21:14:34 +0100

    More_test_cases_and_README

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # MoonLander
+
+## Beskrivelse
+Dette prosjektet er et enkelt moonlander spill laget i C++. Spillet er et eksamensprosjekt som inngår i en 
+mappeeksamensvurdering i faget AIS1003 ved NTNU (Norges tekniske og naturvitenskaplige universitet)
+Meningen og formålet med spillet var å skaffe oss erfaring innen objektorientert programmering.
+Spillet bruker flere klasser for å "simulere" en månelanding, inkludert fysikk, kollisjonsdeteksjon, og brukergrensesnitt. 
+Grafikken er håndtert av`threepp` biblioteket. 
+
+## Forutsetninger
+- **CLion**: Anbefalt å åpne og kjøre prosjektet med CLion IDE.
+- **C++ Standard**: Prosjektet bruker C++20.
+- **Krav**:
+    - [threepp](https://github.com/markaren/threepp.git) for 3D-grafikk.
+    - [imgui](https://github.com/ocornut/imgui.git) for brukergrensesnitt.
+    - [Catch2](https://github.com/catchorg/Catch2.git) for testing.
+
+## Installasjon og Oppsett
+1. Klon prosjektet fra ditt Git-repository.
+2. Åpne prosjektet i CLion (anbefalt).
+3. Sørg for at alle nødvendige biblioteker er installert.
+
+## Bygging og Kjøring
+1. Bygg prosjektet ved å bruke CLion's byggeverktøy.
+2. Kjør prosjektet direkte i CLion, hvor hovedkoden ligger i `main.cpp`.
+
+## Bruk
+- Bruk piltastene til å kontrollere romskipet under landing.
+- `R`-tasten kan brukes for å tilbakestille spillet.
+- Spillet gir en grafisk fremstilling av et månelandskap, hvor spilleren skal lande et romskip.
+
+## Testing
+- Testene er skrevet med `catch2` som rammeverk og kan kjøres for validering av logikken i spillet.
+- Testene er lokalisert i `test.cpp`.
+
+
+## Skapere
+Per Fugelli Lillefosse og Sebastian Myre.
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN
 
 #include <catch2/catch_all.hpp>
+#include <cmath>
 
 #include "AABB.hpp"
 #include "GJKcomputations.hpp"
@@ -8,9 +9,11 @@
 #include "Spaceship.hpp"
 #include "Landscape.hpp"
 #include "UI.hpp"
-#include <cmath>
+#include "LunarLanderGame.hpp"
 
-//Testcases er inspirert fra Chat-gpt og endret etter behov
+
+
+//Testcases er for det meste fra Chat-gpt og endret etter behov.
 
 // Test for AABB class
 TEST_CASE("AABB position and size are set correctly", "[AABB]") {
@@ -67,6 +70,51 @@ TEST_CASE("GJKengine vector operations", "[GJKengine]") {
     }
 }
 
+//Test for Landscape class
+TEST_CASE("Landscape Construction", "[Landscape]") {
+    Landscape landscape(100, 50);
+
+    SECTION("Initial Line Position and Scale") {
+        auto lines = landscape.getLines();
+        REQUIRE(lines.size() == 1);
+        REQUIRE(lines[0].position[0] == Catch::Approx(100));
+        REQUIRE(lines[0].position[1] == Catch::Approx(50));
+        REQUIRE(lines[0].scale[0] == Catch::Approx(0));
+        REQUIRE(lines[0].scale[1] == Catch::Approx(0));
+    }
+}
+
+TEST_CASE("Adding Lines to Landscape", "[Landscape]") {
+    Landscape landscape(100, 50);
+    landscape.addLine(10, 0.0f); // Add a horizontal line
+
+    SECTION("Line Added Correctly") {
+        auto lines = landscape.getLines();
+        REQUIRE(lines.size() == 2);
+        REQUIRE(lines[1].scale[0] == Catch::Approx(10));
+        REQUIRE(lines[1].angle == Catch::Approx(0.0f));
+    }
+
+    SECTION("Correct Total Length") {
+        REQUIRE(landscape.getLength() == Catch::Approx(10));
+    }
+}
+
+//Test for lunar lander game class
+TEST_CASE("Game Update and Response to Input", "[LunarLanderGame]") {
+    LunarLanderGame game;
+    auto& ui = game.getUI();
+
+    SECTION("Respond to Forward Command") {
+        // Simulate pressing the 'UP' key
+        ui.onKeyPressed(threepp::KeyEvent(threepp::Key::UP, true, false));
+        REQUIRE(ui.searchCommands("FORWARD") == true);
+
+        // Update the game
+        game.update(0.016); // Assuming a frame update (60 FPS)
+    }
+}
+
 // Test for PhysicsEngine class
 TEST_CASE("PhysicsEngine movement calculation", "[PhysicsEngine]") {
     PhysicsEngine physics(0, 0);
@@ -98,9 +146,54 @@ TEST_CASE("PhysicsEngine movement calculation", "[PhysicsEngine]") {
 // Test for UI class
 TEST_CASE("UI command registration", "[UI]") {
     UI ui;
-    ui.onKeyPressed(threepp::KeyEvent(threepp::Key::UP, true, false));
 
-    REQUIRE(ui.searchCommands("FORWARD") == true);
+    SECTION("Test UP Key") {
+        ui.onKeyPressed(threepp::KeyEvent(threepp::Key::UP, true, false));
+        REQUIRE(ui.searchCommands("FORWARD") == true);
+    }
+
+    SECTION("Test DOWN Key") {
+        ui.onKeyPressed(threepp::KeyEvent(threepp::Key::DOWN, true, false));
+        REQUIRE(ui.searchCommands("SPEEDUP") == true);
+    }
+
+    SECTION("Test LEFT Key") {
+        ui.onKeyPressed(threepp::KeyEvent(threepp::Key::LEFT, true, false));
+        REQUIRE(ui.searchCommands("LEFT") == true);
+    }
+
+    SECTION("Test RIGHT Key") {
+        ui.onKeyPressed(threepp::KeyEvent(threepp::Key::RIGHT, true, false));
+        REQUIRE(ui.searchCommands("RIGHT") == true);
+    }
+
+    SECTION("Test R Key") {
+        ui.onKeyPressed(threepp::KeyEvent(threepp::Key::R, true, false));
+        REQUIRE(ui.searchCommands("RESET") == true);
+    }
+
+    SECTION("Test Key Release") {
+        ui.onKeyPressed(threepp::KeyEvent(threepp::Key::UP, true, false));
+        ui.onKeyReleased(threepp::KeyEvent(threepp::Key::UP, true, false));
+        REQUIRE(ui.searchCommands("FORWARD") == false);
+    }
+
+    SECTION("Test Multiple Key Presses") {
+        ui.onKeyPressed(threepp::KeyEvent(threepp::Key::UP, true, false));
+        ui.onKeyPressed(threepp::KeyEvent(threepp::Key::LEFT, true, false));
+        REQUIRE(ui.searchCommands("FORWARD") == true);
+        REQUIRE(ui.searchCommands("LEFT") == true);
+    }
+
+    SECTION("Test Command Absence") {
+        ui.onKeyPressed(threepp::KeyEvent(threepp::Key::UP, true, false));
+        REQUIRE(ui.searchCommands("SPEEDUP") == false);
+    }
+
+    SECTION("Test Command Clearing") {
+        ui.onKeyPressed(threepp::KeyEvent(threepp::Key::UP, true, false));
+        ui.onKeyReleased(threepp::KeyEvent(threepp::Key::UP, true, false));
+        REQUIRE(ui.searchCommands("FORWARD") == false);
+    }
+
 }
-
-
